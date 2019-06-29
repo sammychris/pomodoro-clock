59a2da71417d73ebd91ce2e2001c9ac092ed05a5
Samuel Christopher <Ebusameric@gmail.com>
2019-06-29 21:05:41 +0100

    Updates

---
+++ precache-manifest.86a6f287451b00e6dc296c6a13444e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed8484083477dcb2fadd89f350b5ba8",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "9483ca6b4d5d32e1b679",
+    "url": "/pomodoro/static/css/main.2deec00a.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/pomodoro/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "9483ca6b4d5d32e1b679",
+    "url": "/pomodoro/static/js/main.5dd5407c.chunk.js"
+  },
+  {
+    "revision": "8d8194bde761299ff809",
+    "url": "/pomodoro/static/js/runtime~main.5f1f8454.js"
+  }
+]);
